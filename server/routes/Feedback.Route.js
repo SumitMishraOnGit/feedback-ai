54df161673cfc7cf9093f1417c86d0e58b18c20c
SumitMishraonGit <sumitmishra9606@gmail.com>
2025-08-08 00:58:55 +0530

    feat: added model and basic routing

--- server/routes/Feedback.Route.js
+++ server/routes/Feedback.Route.js
@@ -1,17 +1,12 @@
 const express = require('express');
-const port = 3000;
 const router = express.Router();
 
-router.get('/api/feedback', (req, res) => {
+router.get('/', (req, res) => {
       res.send().statusCode(200);
 });
 
-router.post('/api/feedback', (req, res) => {
+router.post('/', (req, res) => {
       res.send().statusCode(200);
 });
 
-router.listen(port, () => {
-      console.log(`listening on port ${port}`);
-});
-
-module.exports = Routes;
+module.exports = router;
